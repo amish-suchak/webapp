a187cfc42f43a75c7985328ae31b7f0939612c77
Amish Suchak <amish.suchak@gmail.com>
2023-03-10 13:30:47 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return("<H1>Hello, World from me!</H1>")
+
+if __name__=='__main__':
+     app.run()
